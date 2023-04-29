8bc9e06b5ff095448768fd90c66079edf766fafe
Andre <andrebucelli@gmail.com>
2023-04-29 11:15:52 +0200

    debug1

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ let type = 'search.php?s=';
 let research;
 let search = document.getElementById('search');
 
+search.addEventListener("keydown", function (e) {
+    if (e.code == "Enter") e.preventDefault();
+});
+
 // https://www.thecocktaildb.com/api/json/v1/1/random.php
 
 function searchType(n) {
@@ -22,7 +26,7 @@ function searchType(n) {
 }
 
 
-function popolateCard() {
+function popolateCard(e) {
     var i = 0;
     research = 'https://www.thecocktaildb.com/api/json/v1/1/' + type + search.value + '';
     document.getElementById("mainPage").remove();
@@ -35,10 +39,9 @@ function popolateCard() {
                     document.getElementById("caricamento").className = "";
                     document.getElementById('tab').innerHTML =
                         `<div class="col">
+                        <a data-bs-toggle="modal" data-bs-target="#exampleModal"> 
                             <div class="card h-100" onclick = "popolateModal(`+ index + `)" >
-                            <a data-bs-toggle="modal" data-bs-target="#exampleModal"> 
                             <img id="image` + index + `" class="card-img-top h-45" alt="...">
-                            </a>
                                 <div class="card-body">
                                     <h5 class="card-title" id="name` + index + `"></h5>
                                     <p class="card-text" id="description` + index + `">This is a longer card with supporting text below as a natural lead-in to
@@ -48,6 +51,7 @@ function popolateCard() {
                                     <small id="cardFooter` + index + `" class="text-muted">Last update </small>
                                 </div>
                             </div>
+                            </a>
                         </div>`;
 
                     document.getElementById("image" + index).src = response.drinks[index].strDrinkThumb;
@@ -57,10 +61,9 @@ function popolateCard() {
                 } else {
                     document.getElementById('tab').innerHTML +=
                         `<div class="col">
-                            <div class="card h-100" onclick = "popolateModal(`+ index + `)" >
                             <a data-bs-toggle="modal" data-bs-target="#exampleModal"> 
+                            <div class="card h-100" onclick = "popolateModal(`+ index + `)" >
                             <img id="image` + index + `" class="card-img-top h-45" alt="...">
-                            </a>
                                 <div class="card-body">
                                     <h5 class="card-title" id="name` + index + `"></h5>
                                     <p class="card-text" id="description` + index + `">This is a longer card with supporting text below as a natural lead-in to
@@ -70,6 +73,7 @@ function popolateCard() {
                                     <small id="cardFooter` + index + `" class="text-muted">Last update </small>
                                 </div>
                             </div>
+                            </a>
                         </div>`;
 
                     console.log(response.drinks[index].strDrink);
